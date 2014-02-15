93f7d88cfd5d47b0a08d6fa125e4934643bbbafd
Dca <dca35311@gmail.com>
2014-02-15 22:23:35 +0800

    Add grunt watch package

--- gruntfile.js
+++ gruntfile.js
@@ -20,6 +20,7 @@ module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-gh-pages');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.registerTask('publish', ['copy', 'gh-pages']);
 
