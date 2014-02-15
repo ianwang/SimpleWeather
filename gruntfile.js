4dd005043b1b3423d7597b6227950583cfd6b82a
Dca <dca35311@gmail.com>
2014-02-15 17:53:17 +0800

    move file

--- gruntfile.js
+++ gruntfile.js
@@ -1,7 +1,24 @@
 module.exports = function (grunt) {
 
     grunt.initConfig({
-
+        'gh-pages': {
+            options: {
+                base: 'build'
+            },
+            src: '**/*'
+        },
+        copy: {
+            main: {
+                src: 'src/**/*',
+                dest: 'build/',
+            },
+        },
     });
+
+    grunt.loadNpmTasks('grunt-gh-pages');
+
+    grunt.registerTask('publish', ['copy', 'gh-pages']);
+
+    grunt.registerTask('copy', ['copy']);
 
 };
