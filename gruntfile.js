7fdc1d6255a3494d837649d8517f4c4207061d11
Dca <dca35311@gmail.com>
2014-02-16 23:11:26 +0800

    Update grunt

--- gruntfile.js
+++ gruntfile.js
@@ -8,13 +8,40 @@ module.exports = function (grunt) {
             src: '**/*'
         },
         copy: {
-            main: {
-                expand: true,
-                cwd: 'src/',
-                src: '**',
-                dest: 'build/',
-                filter: 'isFile',
+            components: {
+                files: [{
+                        expand: true,
+                        cwd: 'src/',
+                        src: ['components/**/*.min.js', 'components/**/*.min.js.map'],
+                        dest: 'build/'
+                    },{
+                        expand: true,
+                        cwd: 'src/',
+                        src: 'components/foundation/css/**.css',
+                        dest: 'build/'
+                    },{
+                        expand: true,
+                        cwd: 'src/',
+                        src: 'public/images/**/*',
+                        dest: 'build/'
+                    },
+
+                ]
+            }
+        },
+        uglify: {
+            options: {
+                compress: false,
+                beautify: true,
+                mangle: false,
+                sourceMap: 'build/public/javascripts/main.min.js.map'
+
             },
+            my_target: {
+                files: {
+                    'build/public/javascripts/main.min.js': ['src/public/javascripts/app/**/*.js']
+                }
+            }
         },
         jade: {
             compile: {
@@ -22,15 +49,17 @@ module.exports = function (grunt) {
                     pretty: true
                 },
                 files: {
-                    'src/index.html': 'src/views/index.jade'
+                    'build/index.html': 'src/views/index.jade'
                 }
             }
         },
-        compass:{
-            dev: {
+        sass: {
+            main: {
                 options: {
-                    sassDir: 'src/public/stylesheets/sass/main',
-                    cssDir: 'src/public/stylesheets/css'
+                    compass: true
+                },
+                files: {
+                    'build/public/stylesheets/style.css': 'src/public/stylesheets/sass/main/style.sass'
                 }
             }
         },
@@ -43,7 +72,7 @@ module.exports = function (grunt) {
                 }
             },
             compass: {
-                files: ['src/public/stylesheets/sass//**/*.sass'],
+                files: ['src/public/stylesheets/sass/**/*.sass'],
                 tasks: ['compass'],
                 options: {
                     //
@@ -56,8 +85,14 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jade');
-    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    // grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('publish', ['copy', 'gh-pages']);
+    grunt.registerTask('publish', ['build', 'gh-pages']);
+
+    grunt.registerTask('default', ['build', 'watch']);
+
+    grunt.registerTask('build', ['uglify', 'copy', 'jade', 'sass']);
 
 };
